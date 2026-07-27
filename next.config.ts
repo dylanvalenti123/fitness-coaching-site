2cfde804ffecc01a4ea485bc28189860d1fc282b
Dylan Valenti <dylan@Mac.localdomain>
2026-07-26 20:09:03 -0400

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["127.0.0.1", "localhost"],
-  turbopackMemoryLimit: 4 * 1024 * 1024 * 1024, // 4GB
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
